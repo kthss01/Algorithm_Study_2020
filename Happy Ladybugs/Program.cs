3db47d1c65484e2ebe14475b94bf1403698ef540
김태훈 <kthss01@naver.com>
2020-03-25 13:34:56 +0900

    c# study

---
+++ Happy Ladybugs/Program.cs
@@ -0,0 +1,101 @@
+﻿// Happy Ladybugs
+/*
+ * ladybug 무당벌레
+ * 
+ * 규칙
+ * 무당벌레는 같은 색의 무당벌레와 붙어있으면 행복해짐
+ * 대문자로 구분하고 빈 곳은 _로 표시
+ * 빈 곳으로 무당벌레를 옮길 수 있음
+ * 모든 무당벌레가 행복할 수 있는지 체크하기
+ * 
+ * ex)
+ * b = [YYR_B_BR] -> [YYRRBB__] 가 되어서 행복할 수 있음
+ * 1 <= g, n <= 100 g 게임 수, n 보드의 공간 수
+ * b[i] _, A ~ Z
+ */
+
+using System;
+
+namespace Happy_Ladybugs
+{
+    class Program
+    {
+        static string happyLadybugs(string b)
+        {
+            bool result = true;
+
+            if (b.Contains("_"))
+            {
+                int[] alphabets = new int[27];
+
+                for(int i=0; i<b.Length; i++)
+                {
+                    if (b[i] == '_')
+                        alphabets[26]++;
+                    else
+                        alphabets[b[i] - 'A']++;
+                }
+
+                for (int i = 0; i < 26; i++)
+                {
+                    if (alphabets[i] == 1)
+                    {
+                        result = false;
+                        break;
+                    }
+                
+                }
+            }
+            else
+            {
+                int cnt = 0;
+                char temp = '_';
+
+                for (int i=0; i<b.Length; i++)
+                {
+                    if (i == 0 && b[i] != '_')
+                    {
+                        cnt = 1;
+                        temp = b[i];
+                    }
+                    else if (b[i] == temp)
+                    {
+                        cnt++;
+                    }
+                    else
+                    {
+                        if (cnt == 1)
+                        {
+                            result = false;
+                            break;
+                        }
+                        else
+                        {
+                            cnt = 1;
+                            temp = b[i];
+                        }
+                    }
+                }
+
+                if (cnt == 1)
+                    result = false;
+            }
+
+            return result ? "YES" : "NO";
+        }
+
+        static void Main(string[] args)
+        {
+            int g = Convert.ToInt32(Console.ReadLine());
+
+            for (int gItr = 0; gItr < g; gItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                string b = Console.ReadLine();
+
+                string result = happyLadybugs(b);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Strange Counter/Program.cs
@@ -0,0 +1,87 @@
+﻿// Strange Counter
+/*
+ * 규칙
+ * 처음엔 3으로 시작
+ * 1씩 내려가다 1이되면 리셋되며
+ * 리셋될 때 이전 사이클의 처음 값의 x 2
+ * 
+ * ex)
+ * 3 2 1
+ * 6 5 4 3 2 1
+ * 12 11 10 9 8 7 6 5 4 3 2 1 ....
+ * 
+ * t가 주어질 때 값 구하기
+ * 
+ * 1 <= t <= 10^12
+ */
+
+ /*
+  * 그냥 반복문 돌리면 시간초과
+  * 
+  * 식
+  * t = 1,4,10,22,46 이렇게 간다 치면
+  * m = 0, 1, 2, ...
+  * 
+  * 3x2^m - 2 = t
+  * -> 이걸 다시 m에 대해 풀면
+  * m = log2((t+2)/3)
+  * m = (int)log2((t+2)/3)
+  * 
+  * v = 3, 6, 12, 24
+  * v = 3x2^m (m = 0,1,2...)
+  * 
+  * 정답은 v - (t - (3x2^m - 2)) <- 차이빼는거
+  */
+
+using System;
+
+namespace Strange_Counter
+{
+    class Program
+    {
+        static long strangeCounter(long t)
+        {
+            //long p = 3;
+            //long nextT = 1;
+            //
+            //while (true)
+            //{
+            //    nextT += p;
+            //    if (t < nextT)
+            //    {
+            //        nextT -= p;
+            //        break;
+            //    }
+            //    p *= 2;
+            //}
+            //
+            //for (int i = 0; i < t - nextT; i++)
+            //    p--;
+            //
+            //return p;
+
+            // 이런 방법도 있음
+            long rem = 3;
+            while (t > rem) 
+            {
+                t = t - rem;
+                rem *= 2;
+            }
+            return rem - t + 1;
+
+            // 내가 식 만든 거
+            long m = (long)Math.Log2((t + 2) / 3);
+            long v = 3 * (long)Math.Pow(2, m);
+
+            return v - (t - (3 * (long)Math.Pow(2, m) - 2));
+        }
+
+        static void Main(string[] args)
+        {
+            long t = Convert.ToInt64(Console.ReadLine());
+
+            long result = strangeCounter(t);
+            Console.WriteLine(result);
+        }
+    }
+}
